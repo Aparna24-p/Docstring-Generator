27c37fdff37e32921e464ac6b52f92e07aa3a5b5
Aparna24-p <aparnapathak2004@gmail.com>
2026-01-05 06:43:31 +0000

    Initial commit from Colab

---
+++ data_processor.py
@@ -0,0 +1,11 @@
+def calculate_average(numbers: list) -> float:
+    """Existing docstring to be validated."""
+    return sum(numbers) / len(numbers)
+
+class DataProcessor:
+    def __init__(self, data: list):
+        self.data = data
+        
+    def process_item(self, item: str, index: int = 0):
+        # This function is missing a docstring!
+        pass
