32dd8754de49f00dd3b7c3098da3aec2e2af737b
Aparna Pathak <aparnapathak2004@gmail.com>
2026-02-18 17:31:17 +0530

    Clean up: Remove temporary upload files to fix CI docs check

--- temp_upload.py
+++
@@ -1,8 +0,0 @@
-@st.cache_data  # Decorated function
-def outer_func(a):
-    def inner_func(b):  # Nested function
-        return b * 2
-    return inner_func(a)
-
-class EmptyClass:  # Class without methods
-    pass
